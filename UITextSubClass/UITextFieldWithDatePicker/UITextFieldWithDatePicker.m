c4b4db1378f936bf2b3b925498e4b875af667658
akuraru <akuraru+bb@gmail.com>
2013-10-01 09:55:07 +0900

    メニューが出て、貼り付けされると表示との齟齬を起こすのでやらせない

--- UITextSubClass/UITextFieldWithDatePicker/UITextFieldWithDatePicker.m
+++ UITextSubClass/UITextFieldWithDatePicker/UITextFieldWithDatePicker.m
@@ -140,4 +140,13 @@
     return keyboardDoneButtonView;
 }
 
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
+	UIMenuController *menuController = [UIMenuController sharedMenuController];
+	if (menuController) {
+		[UIMenuController sharedMenuController].menuVisible = NO;
+	}
+    
+	return NO;
+}
+
 @end
